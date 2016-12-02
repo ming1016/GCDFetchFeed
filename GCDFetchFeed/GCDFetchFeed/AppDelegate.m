38887a3285e278a97c7c20c26d0f0821fc27f6e0
daiming <daiming@didichuxing.com>
2016-12-02 19:33:06 +0800

    底部切换去掉

--- GCDFetchFeed/GCDFetchFeed/AppDelegate.m
+++ GCDFetchFeed/GCDFetchFeed/AppDelegate.m
@@ -25,38 +25,40 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     //首页
     SMRootViewController *rootVC = [[SMRootViewController alloc] init];
+//    UINavigationController *homeNav = [self styleNavigationControllerWithRootController:rootVC];
     UINavigationController *homeNav = [self styleNavigationControllerWithRootController:rootVC];
-    UITabBarItem *homeTab = [[UITabBarItem alloc] initWithTitle:@"频道" image:nil tag:1];
-    homeTab.titlePositionAdjustment = UIOffsetMake(0, -20);
-    homeNav.tabBarItem = homeTab;
+//    UITabBarItem *homeTab = [[UITabBarItem alloc] initWithTitle:@"频道" image:nil tag:1];
+//    homeTab.titlePositionAdjustment = UIOffsetMake(0, -20);
+//    homeNav.tabBarItem = homeTab;
+//    
+//    //列表
+//    SMFeedModel *feedModel = [SMFeedModel new];
+//    feedModel.fid = 0;
+//    SMFeedListViewController *feedListVC = [[SMFeedListViewController alloc] initWithFeedModel:feedModel];
+//    UINavigationController *listNav = [self styleNavigationControllerWithRootController:feedListVC];
+//    UITabBarItem *listTab = [[UITabBarItem alloc] initWithTitle:@"列表" image:nil tag:2];
+//    listTab.titlePositionAdjustment = UIOffsetMake(0, -18);
+//    listNav.tabBarItem = listTab;
+//    
+//    //map
+//    SMMapViewController *mapVC = [[SMMapViewController alloc] init];
+//    UINavigationController *mapNav = [self styleNavigationControllerWithRootController:mapVC];
+//    UITabBarItem *mapTab = [[UITabBarItem alloc] initWithTitle:@"地图" image:nil tag:2];
+//    mapTab.titlePositionAdjustment = UIOffsetMake(0, -18);
+//    mapNav.tabBarItem = mapTab;
+//    
+//    UITabBarController *tabBarC = [[UITabBarController alloc]initWithNibName:nil bundle:nil];
+//    tabBarC.tabBar.tintColor = [SMStyle colorPaperBlack];
+//    tabBarC.tabBar.barTintColor = [SMStyle colorPaperDark];
+//    UIView *shaowLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(tabBarC.tabBar.frame), 0.5)];
+//    shaowLine.backgroundColor = [UIColor colorWithHexString:@"D8D7D3"];
+//    [tabBarC.tabBar addSubview:shaowLine];
+//    tabBarC.tabBar.shadowImage = [UIImage new];
+//    tabBarC.tabBar.clipsToBounds = YES;
+//    tabBarC.viewControllers = @[homeNav,listNav,mapNav];
     
-    //列表
-    SMFeedModel *feedModel = [SMFeedModel new];
-    feedModel.fid = 0;
-    SMFeedListViewController *feedListVC = [[SMFeedListViewController alloc] initWithFeedModel:feedModel];
-    UINavigationController *listNav = [self styleNavigationControllerWithRootController:feedListVC];
-    UITabBarItem *listTab = [[UITabBarItem alloc] initWithTitle:@"列表" image:nil tag:2];
-    listTab.titlePositionAdjustment = UIOffsetMake(0, -18);
-    listNav.tabBarItem = listTab;
-    
-    //map
-    SMMapViewController *mapVC = [[SMMapViewController alloc] init];
-    UINavigationController *mapNav = [self styleNavigationControllerWithRootController:mapVC];
-    UITabBarItem *mapTab = [[UITabBarItem alloc] initWithTitle:@"地图" image:nil tag:2];
-    mapTab.titlePositionAdjustment = UIOffsetMake(0, -18);
-    mapNav.tabBarItem = mapTab;
-    
-    UITabBarController *tabBarC = [[UITabBarController alloc]initWithNibName:nil bundle:nil];
-    tabBarC.tabBar.tintColor = [SMStyle colorPaperBlack];
-    tabBarC.tabBar.barTintColor = [SMStyle colorPaperDark];
-    UIView *shaowLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(tabBarC.tabBar.frame), 0.5)];
-    shaowLine.backgroundColor = [UIColor colorWithHexString:@"D8D7D3"];
-    [tabBarC.tabBar addSubview:shaowLine];
-    tabBarC.tabBar.shadowImage = [UIImage new];
-    tabBarC.tabBar.clipsToBounds = YES;
-    tabBarC.viewControllers = @[homeNav,listNav,mapNav];
-    
-    self.window.rootViewController = tabBarC;
+//    self.window.rootViewController = tabBarC;
+    self.window.rootViewController = homeNav;
     [self.window makeKeyAndVisible];
     return YES;
 }
